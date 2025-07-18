5b20c132bee96780277ef239b72e00c19dd199c8
Karthick Muthukrishnan <karthick.muthukrishnan@cognizant.com>
2025-07-18 10:05:19 +0100

    sonar change
    code change to exclude the interface

--- AqieHistoricaldataBackend/Atomfeed/Services/IAnnualAtomFeedExportCSV.cs
+++ AqieHistoricaldataBackend/Atomfeed/Services/IAnnualAtomFeedExportCSV.cs
@@ -3,8 +3,10 @@ using static AqieHistoricaldataBackend.Atomfeed.Models.AtomHistoryModel;
 
 namespace AqieHistoricaldataBackend.Atomfeed.Services
 {    
+    
     public interface IAnnualAtomFeedExportCSV
-    {        
+    {
+        [ExcludeFromCodeCoverage]
         Task<byte[]> annualatomfeedexport_csv(List<FinalData> Final_list, QueryStringData data);
     }
 }
